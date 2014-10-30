bb338d09190312b3ea5745ff65a3483a0ecd3862
Eric Chan <tormchan@gmail.com>
2014-10-30 01:03:05 -0700

    Use SSL and Unicorn in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,6 +42,7 @@ Rails.application.configure do
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
+  config.force_ssl = true
 
   # Decrease the log volume.
   # config.log_level = :info
